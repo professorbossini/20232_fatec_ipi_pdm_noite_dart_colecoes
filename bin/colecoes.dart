e265bf37e4c0aeeadb29fb89cc713ade10f3dae5
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-01 22:59:13 -0300

    ap03(colecoes): proposta do jogo da mega sena

--- bin/colecoes.dart
+++ bin/colecoes.dart
@@ -1,5 +1,39 @@
-import 'package:colecoes/colecoes.dart' as colecoes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${colecoes.calculate()}!');
+void main(){
+  
 }
+
+// void main(List<String> arguments) {
+//   var lista = ['Ana', 'Pedro', 'Ana'];
+//   lista.removeWhere((element) => element == "Ana");
+//   print(lista);
+//   //type annotation
+//   // var somenteStrings = <String>[];
+//   // List lista = [];
+//   // print(lista.runtimeType);
+//   // lista.add(true);
+//   // print(lista.runtimeType);
+//   // lista.add("Ana");
+//   // print(lista.runtimeType);
+//   // List<String> nomes = ['Ana'];
+//   // List<bool> maiores = [true, false, true];
+//   // var nomes = ['Ana', 'João', 'Maria'];
+//   // nomes.add('Cristina');
+//   // nomes.insert(0, 'Ana Maria');
+//   // nomes.insert(2, 'Pedro');
+//   // nomes.insert(6, 'Rodrigo');
+//   // nomes.insert(nomes.length, 'José');
+//   // print(nomes);
+
+//   // var itensDiversos = [2, 2.5];
+//   // print(itensDiversos.runtimeType);
+//   //escreva um programa que some os numeros passados como argumentos na linha de comando
+//   //e mostre o resultado
+//   // var acumulador = 0;
+//   // for (final valor in arguments) {
+//   //   acumulador = acumulador + int.parse(valor);
+//   // }
+//   // print(acumulador);
+//   // print(arguments
+//   //     .map((e) => int.parse(e))
+//   //     .reduce((value, element) => value + element));
+// }

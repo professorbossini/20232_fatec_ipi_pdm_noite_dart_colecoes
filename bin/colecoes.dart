fe4f285b21fbb0947aa0345af681dab259deb790
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-15 20:37:15 -0300

    ap03(colecoes): fim da secao 2.5, antes do exercicio

--- bin/colecoes.dart
+++ bin/colecoes.dart
@@ -1,6 +1,160 @@
-void main(){
-  
+void main() {
+  var pessoa = <String, Object>{'nome': "Pedro", 'idade': 22};
+
+  //keys
+  final keys = pessoa.keys;
+  for (final key in keys) {
+    print(key);
+    print(pessoa[key]);
+  }
+  //values
+  final values = pessoa.values;
+  for (final value in values) {
+    print(value);
+  }
+  //entries
+  final entries = pessoa.entries;
+  for (final entry in entries) {
+    print(entry);
+    print(entry.key);
+    print(entry.value);
+  }
+
+  // var pessoa = <String, dynamic>{'nome': 'Pedro', 'idade': 22};
+  // print(pessoa.runtimeType);
+  // var nome = pessoa['idade'] as String;
+  // nome.
+  // print(pessoa['nome completo']);
+  // print(pessoa.nome completo);
+  // var pessoa = {'nome': 'Ana', 'idade': 22};
+  // print(pessoa['nome']);
+  // print(pessoa.nome);não tem ponto para mapas
+  // type annotation
+  // var pessoa = <String, String>{"nome": "Ana"};
+  // Map<String, String> pessoa2 = {};
+  // // pessoa.addAll({'nome': 'Pedro', 'sobrenome': 'Silva'});
+  // print(pessoa);
+
+  // var lembretes = <String> {1: "Fazer compras", 2: 'ver um filme'};
 }
+
+// void main(List<String> arguments) {
+// var portugues = {'Brasil', 'Portugal'};
+// var europa = {'Alemanha', 'Portugal', 'Espanha'};
+
+ 
+
+  
+//   var pais1 = portugues.union(europa);
+
+//   print ("Todos os paises que falam portugues e todos da europa");
+//   print(pais1);
+
+//   var pais2 = portugues.intersection(europa);
+
+//   print ("Todos os países em que se fala português e que são europeus.");
+//   print(pais2);
+
+//   var pais3 = portugues.difference(europa);
+
+//   print ("Todos os países em que se fala português e que não são europeus.");
+//   print(pais3);
+
+
+// }
+
+// void main() {
+//   var A = {1, 2, 3, 4, 5, 6};
+//   var B = {1, 3, 7};
+//   print(A.union(B)); //1, 2, 3, 4, 5, 6, 7
+//   print(A.intersection(B)); //1, 3
+//   print(A.difference(B)); //2, 4, 5, 6
+//   print(B.difference(A)); //7
+//   print(A.difference(A)); //{}
+
+//   print((A.union(B)).difference(A.intersection(B)));
+// }
+
+
+// void main() {
+//   var nomes = {'Pedro', 'João'};
+//   print(nomes.elementAt(0));
+
+//   //for each
+//   for (var nome in nomes) {
+//     print(nome);
+//   }
+
+//   for (var i = 0; i < nomes.length; i++) {
+//     print(nomes.elementAt(i));
+//   }
+// }
+
+// void main() {
+//   // [1, 2] != [2, 1] != [1, 2, 2]
+//   //{1, 2} = {2, 1} = {1, 2, 2} = {2, 2, 2, 2, 2, 1, 1, 2, 1, 2, 1}
+//   var nomes = {'Ana', 'João'};
+//   print(nomes);
+//   print(nomes.runtimeType);
+
+//   var paises = {'Brasil', 'Brasil'};
+//   print(paises);
+
+//   var oQueEhIsso = {}; //isso é um mapa
+//   print(oQueEhIsso.runtimeType);
+// }
+
+
+// void main() {
+//   var tupla = ('Ana', 18, true);
+//   print(tupla);
+//   print(tupla.runtimeType);
+
+//   print(tupla.$1);
+//   print(tupla.$2);
+//   // print(tupla.$4);
+// }
+
+// void main(){
+//   // List<int> lista = [1, 2, 3];
+
+//   // List < List <int> > matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+//   // List < List < List <int> > > cubo = [[[1, 2],[3, 4]], [[5, 6],[7, 8]]];
+
+//   // List<List<List>> a;
+
+//   dynamic b = "abc";
+//   b.falar();
+
+// }
+
+// void main() {
+  // List<List<List<int?>?>?>? lista;
+
+  // List<String?>? lista = [null];
+  // List<String>? eAGora = [null];
+  // List<String?> nomes4 = null;
+  // List<String> nomes3 = [];
+  // nomes3.add(null);
+  // var nomes2 = [];
+  // print(nomes2.runtimeType);
+  // nomes2.add(null);
+  // nomes2.add(1);
+  // nomes2.add(true);
+  // var nomes1 = ["Ana", "Pedro", null];
+  // print(nomes1.runtimeType);
+  // nomes1.add(null);
+
+  // const nomes = ["Ana", "João"];
+  // nomes[0] = "PEdro";
+
+  // declaração de coleções com final
+  // final nomes = ['Ana', 'Pedro'];
+  // // nomes = [];
+  // nomes[0] = "Ana Paula";
+  // nomes.add("Jaqueline");
+// }
 
 // void main(List<String> arguments) {
 //   var lista = ['Ana', 'Pedro', 'Ana'];
